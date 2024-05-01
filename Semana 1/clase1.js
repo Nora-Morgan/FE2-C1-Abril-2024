caa76e50d642e5be0015c546faab978726eeba1a
Nora-Morgan <100365023+Nora-Morgan@users.noreply.github.com>
2024-05-01 20:07:48 -0300

    Clase1-2-Extras

--- Semana 1/clase1.js
+++ Semana 1/clase1.js
@@ -51,12 +51,12 @@
       do{
             console.log(nombre);
             if(nombreSoloLetras.test(nombre)){
-                  console.log("El nombre tiene solo letras.")
+                  console.log("El nombre tiene solo letras.");
                   if (nombre.length > 3){
-                        console.log("El nombre es válido.")
+                        console.log("El nombre es válido.");
                         nombreValido = true;
                   } else{
-                        console.log("El nombre tiene 3 caracteres o menos.")
+                        console.log("El nombre tiene 3 caracteres o menos.");
                         nombre = prompt("El nombre ingresado debe contener 4 o más letras.");
                   }
             } else{
@@ -64,7 +64,7 @@
                   if (nombre.length > 3){
                         nombre = prompt("El nombre ingresado debe contener solo letras.");
                   } else{
-                        console.log("El nombre tiene 3 caracteres o menos y algunos no son letras.")
+                        console.log("El nombre tiene 3 caracteres o menos y algunos no son letras.");
                         nombre = prompt("El nombre ingresado debe contener 4 o más letras.");                  }
             }
       } while (!nombreValido); //se vuelve a ejecutar mientras que el nombre no sea válido 
@@ -76,7 +76,7 @@
       alert("Gracias por jugar " + nombre + ". ¡Mucha suerte!");
   
       console.log("----------------------------");
-      console.log("El jugador es:")
+      console.log("El jugador es:");
       console.log(nombre);
       console.log("----------------------------");
 
