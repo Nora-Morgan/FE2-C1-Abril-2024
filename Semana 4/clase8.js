ef52d93e49cd2d6df2e902a65efb611bbf7cdd5c
Nora-Morgan <100365023+Nora-Morgan@users.noreply.github.com>
2024-05-27 20:58:58 -0300

    clase9

--- Semana 4/clase8.js
+++ Semana 4/clase8.js
@@ -116,20 +116,51 @@ sin contar espacios al principio, en el medio o final, sumar el error:
 */
 
 
-// function validarInformacion(usuario) {
-//     let errores = [];
-//     // 👇 desarrollar aqui la funcion
 
-//     return errores;
-// }
+function validarInformacion(usuario) {
+    let errores = [];   
+    if (!isNaN(usuario.nombre) || usuario.nombre.length < 3) {
+        errores.push("El nombre de usuario debe tener mas de 3 caracteres y no puede ser un número")
+    } 
+    if (usuario.password.trim().length < 6) {
+        errores.push("El password debe tener más de 6 caracteres")
+    }
+    if (usuario.telefono.trim().length < 10) {
+        errores.push("El telefono debe tener más de 10 números")
+    }
+    if (usuario.hobbies.length > 4 || usuario.hobbies.length == 0) {
+        errores.push("Solo es posible seleccionar un máximo de 4 hobbies, y como mínimo 1");
+    }
+    if(usuario.nacionalidad == ""){
+        errores.push("Se debe seleccionar una nacionalidad")
+    }
 
-
+    return errores;
+}
 
 /* -------------------------------------------------------------------------- */
 /*                       [4] FUNCION: renderizar errores                      */
 /* -------------------------------------------------------------------------- */
 
+function renderizarErrores(listado) {
+    const cajaErrores = document.querySelector("#errores");
+    console.log(cajaErrores);
 
+    if (cajaErrores) {
+          cajaErrores.remove();
+    }
+
+    if (listado.length > 0) {
+          const divTemplate = document.createElement('div');
+          divTemplate.setAttribute("id", "errores");
+          divTemplate.style = "background:rgba(255, 0, 0, 0.2);padding:.5em 1em;color: red;margin: .5em 0;";
+          listado.forEach( function(error){
+                divTemplate.innerHTML += `<p><span>${error}</span></p>`;
+          })
+
+          form.appendChild(divTemplate);
+    }
+}
 
 /* -------------------------------------------------------------------------- */
 /*                [5] FUNCION: Formulario completado con éxito                */
@@ -144,3 +175,27 @@ sin contar espacios al principio, en el medio o final, sumar el error:
 // 3 - a su vez se debe deshabilitar el boton del formulario
 // 4 - finalmente pasados 4 segundos:
 //     se debe eliminar esa caja, habilitar el botón y limpiar el formulario
+
+function mostrarMensajeExito(listado) {
+
+    if (listado == 0) {
+        const divTemplate = document.createElement('div');
+        divTemplate.setAttribute("id", "exito");
+        divTemplate.style = "background:rgba(0, 255, 0, 0.2);padding:.5em 1em;color: red;margin: .5em 0;";
+        
+        divTemplate.innerHTML = `<p><span>¡Formulario completado con éxito!</span></p>`;
+
+        form.appendChild(divTemplate);
+
+        const boton = document.querySelector("button");
+        boton.setAttribute("disabled", "");
+
+        const cajaExito = document.querySelector("#exito");
+
+        setTimeout(() => {
+            boton.removeAttribute("disabled");
+            form.reset();
+            cajaExito.remove();
+        }, 4000);
+    }
+}
---
+++ Semana 5/Clase-09.js
@@ -0,0 +1,156 @@
+/* -------------------------- estado por defecto ------------------------- */
+const estadoUsuario = {
+    email: "",
+    password: "",
+    rol: "",
+    terminos: false
+};
+
+
+// Serán true solo cuando estén correctos
+const estadoErroresOK = {
+    email: false,
+    password: false,
+    rol: false,
+    terminos: false
+};
+
+/* ---------------------------------- nodos --------------------------------- */
+
+// seleccionar todos los elementos que se necesitan
+const formulario = document.forms[0];
+
+const inputEmail = document.querySelector('#email');
+const inputPassword = document.querySelector('#password');
+const inputRol = document.querySelector('#rol');
+const inputTerminos = document.querySelector('#terminos');
+
+const emailError = document.querySelector('#emailError');
+const passwordError = document.querySelector('#passwordError');
+const rolError = document.querySelector('#rolError');
+const terminosError = document.querySelector('#terminosError');
+
+
+/* -------------------------------------------------------------------------- */
+/*                   [1] FUNCION: mostrar errores al usuario                  */
+/* -------------------------------------------------------------------------- */
+function mostrarErrores() {
+    estadoErroresOK.email ? emailError.classList.remove("visible") : emailError.classList.add("visible");
+
+    estadoErroresOK.password ? passwordError.classList.remove("visible") : passwordError.classList.add("visible")
+    estadoErroresOK.rol ? rolError.classList.remove("visible") : rolError.classList.add("visible")
+    estadoErroresOK.terminos ? terminosError.classList.remove("visible") : terminosError.classList.add("visible")
+}
+
+/* -------------------------------------------------------------------------- */
+/*               [2] FUNCION: actulizar los estados                           */
+/* -------------------------------------------------------------------------- */
+
+
+// 👇 por cada cambio en el formulario
+formulario.addEventListener('change', function () {
+
+    // 👇 actualizar el estado
+    estadoUsuario.email = inputEmail.value;
+    estadoUsuario.password = inputPassword.value;
+    estadoUsuario.rol = inputRol.value;
+    estadoUsuario.terminos = inputTerminos.checked;
+
+    estadoErroresOK.email = validarEmail(estadoUsuario.email);
+    estadoErroresOK.password = validarPassword(estadoUsuario.password);
+    estadoErroresOK.rol = validarRol(estadoUsuario.rol);
+    estadoErroresOK.terminos = validarTerminos(estadoUsuario.terminos);
+  
+    mostrarErrores();
+});
+
+
+/* -------------------------------------------------------------------------- */
+/*                        [3] FUNCIONES: validar campos                       */
+/* -------------------------------------------------------------------------- */
+function validarEmail(email) {
+   
+    let resultado = false;
+   
+    // mail@algo.com
+    // ejemplo de expresion regular
+    let regExp = new RegExp("[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}");
+    if (regExp.test(email)) {
+        resultado = true;
+        // console.log("cumple");
+    }
+
+    return resultado;
+}
+
+function validarPassword(password) {
+
+    let resultado = false;
+
+    if (password.length > 5 && !password.includes(" ")) {  
+        resultado = true;
+        // console.log("cumple");
+    }
+ 
+    return resultado;
+}
+
+function validarRol(rol) {
+
+    let resultado = false;
+
+    if (rol == "frontend" || rol == "backend") {
+        resultado = true;
+        // console.log("cumple");
+    }
+    return resultado;
+}
+
+function validarTerminos(verificacion) {
+
+    let resultado = false;
+
+    if (verificacion) {
+        resultado = true
+        // console.log("cumple");
+    }
+    
+    return resultado;
+}
+
+
+/* -------------------------------------------------------------------------- */
+/*                      [4] FUNCION: escuchamos el submit                     */
+/* -------------------------------------------------------------------------- */
+
+// en el evento submit chequear el estado de errores
+formulario.addEventListener('submit', function (evento) {
+    // prevenimos el comportamiento por defecto
+   evento.preventDefault();
+
+    if (
+        estadoErroresOK.email &&
+        estadoErroresOK.password &&
+        estadoErroresOK.rol &&
+        estadoErroresOK.terminos
+    ) {
+        navegarPaginaExito();
+    }
+
+});
+
+
+/* ----------------------------- MESA DE TRABAJO ---------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                [5] FUNCION: Formulario completado con éxito                */
+/* -------------------------------------------------------------------------- */
+// Esta funcion se va a encargar de realizar la redirección cuando el formulario se complete correctamente.
+// Para eso deberá cumplir con los siguientes requerimientos:
+// 1 - Deshabilitar el botón del formulario.
+// 2 - Esperar 3 segundos para redireccionar a la otra página 
+// 3 - Durante ese tiempo el botón deshabilitado debe mostrar el texto: "Cargando..."
+// 4 - Cuando vaya a la página de 'usuario.html' no se debe permitir que mediante
+//     el botón de "Atrás"(la flechita del navegador) el usuario vuelva a index.
+
+function navegarPaginaExito() {
+}
